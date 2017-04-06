c0b3d22fe7abac6ef1a776fa86549636fb4aaf34
walwoodr <walwoodr@gmail.com>
2017-04-06 10:16:43 -0500

    Add session to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < Sinatra::Base
 
   set :views, Proc.new { File.join(root, "../views/") }
   set :public_dir, "public"
-
+  enable :sessions
+  set :session_secret, "cloche_hat_is_best_hat"
 
   get '/' do
     erb :index
@@ -12,7 +13,7 @@ class ApplicationController < Sinatra::Base
     def is_logged_in?
       !!session[:user_id]
     end
-    
+
     def current_user
       User.find(session[:user_id])
     end

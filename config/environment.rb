39ef4c303cb09d688415a5c49a9b5bbbbd2de865
walwoodr <walwoodr@gmail.com>
2017-04-06 10:43:05 -0500

    Modify environment file

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,4 @@ ActiveRecord::Base.establish_connection(
   :database => "db/capsulize#{ENV['SINATRA_ENV']}.sqlite"
 )
 
-require_relative "../app/controllers/application_controller.rb"
-
-Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
-Dir[File.join(File.dirname(__FILE__), "../app/controllers", "*.rb")].sort.each {|f| require f}
+require_all 'app'

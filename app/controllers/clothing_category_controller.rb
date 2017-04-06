07da0206186878eb901993ff32594a20e0537d0d
walwoodr <walwoodr@gmail.com>
2017-04-06 11:24:52 -0500

    Build controller and views for category actions.

--- app/controllers/clothing_category_controller.rb
+++ app/controllers/clothing_category_controller.rb
@@ -1,4 +1,38 @@
 class ClothingCategoryController < ApplicationController
 
+  #Create
+  get '/categories/new' do
+    redirect_if_not_logged_in do
+      erb :'categories/new'
+    end
+  end
+
+  post '/categories' do
+    #process form input
+  end
+
+  #Read - only read list
+  get '/categories' do
+    redirect_if_not_logged_in do
+      erb :'categories/index'
+    end
+  end
+
+  #Update - update name only
+  get '/categories/:id/edit' do
+    redirect_if_not_logged_in do
+      erb :'categories/edit'
+    end
+  end
+
+  patch '/categories/:id' do
+    # process form input
+  end
+
+  #Delete - only delete if there are no clothes associated.
+
+  delete '/categories/:id' do
+    # process form input
+  end
 
 end
--- config.ru
+++ config.ru
@@ -3,6 +3,7 @@ require 'config/environment'
 
 use Rack::Static, :urls => ['/css'], :root => 'public' # Rack fix allows seeing the css folder.
 
+use ClothingCategoryController
 use ClothingItemController
 use UsersController
 use Rack::MethodOverride

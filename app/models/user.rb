284df2b74935cae24855fc32f47ead11fd35ddca
walwoodr <walwoodr@gmail.com>
2017-04-06 09:58:02 -0500

    User associations and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
 
+  has_many :user_clothing_items
+  has_many :clothing_items, through: :user_clothing_items
+  has_secure_password
+  validates_presence_of :username, :password, :name
+
 end
--- db/migrate/20170404214650_create_users.rb
+++ db/migrate/20170404214650_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       t.text :username
-      t.text :password
+      t.text :password_digest
       t.text :name
     end
   end

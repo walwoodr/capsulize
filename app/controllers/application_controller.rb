05429d06fa82501365947ffeb2b3aa1ac606d371
walwoodr <walwoodr@gmail.com>
2017-04-06 10:30:56 -0500

    Add login form and change location of controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,20 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  get '/login' do
+    erb :login
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    if !!user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/clothes'
+    else
+      redirect '/login'
+    end
+  end
+
   helpers do
     def is_logged_in?
       !!session[:user_id]

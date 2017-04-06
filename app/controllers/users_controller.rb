f6f410e2bc33d817b1aec71da77dc18701911149
walwoodr <walwoodr@gmail.com>
2017-04-06 15:11:28 -0500

    Create views and routes for signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,11 @@ class UsersController < ApplicationController
     erb :'users/index'
   end
 
+  get '/signup' do
+    if is_logged_in?
+      redirect '/'
+    end
+    erb :'users/new'
+  end
+
 end
